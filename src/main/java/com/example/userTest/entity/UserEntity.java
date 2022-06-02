94aad26fa7a80dcab329c3b933b5a68f37fc4459
Egor <307518@niuitmo.ru>
2022-06-03 01:32:36 +0300

    feat: created entities

---
+++ src/main/java/com/example/userTest/entity/UserEntity.java
@@ -0,0 +1,55 @@
+package com.example.userTest.entity;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+    private List<TodoEntity> todoList;
+
+    public UserEntity(){}
+    public UserEntity(Long id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<TodoEntity> getTodoList() {
+        return todoList;
+    }
+
+    public void setTodoList(List<TodoEntity> todoList) {
+        this.todoList = todoList;
+    }
+}
